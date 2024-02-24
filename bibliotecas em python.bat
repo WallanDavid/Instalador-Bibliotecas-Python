30f938ea0c04658259d557cd4951ac3ea3e02d77
Wallan Peixoto <31707595+WallanDavid@users.noreply.github.com>
2024-02-24 14:16:45 -0300

    Add files via upload

---
+++ bibliotecas em python.bat
@@ -0,0 +1,16 @@
+# Script de instalação de bibliotecas Python
+
+pip install jupyter
+pip install spyder
+pip install numpy scipy numba pandas dask
+pip install bokeh holoviews datashader matplotlib seaborn
+pip install scikit-learn h2o tensorflow
+pip install conda
+pip install graph-tool mlpy pytorch
+pip install sagemath scikit-image gekko sympy statsmodels
+pip install plotly pygal keras cntk theano
+pip install google-colab nltk spacy pybrain
+pip install django requests flask bottle pyramid cherrypy tornado vibora dash
+pip install wxpython pyqt pygobject kivy
+pip install tkinter beautifulsoup4 scrapy requests-html lxml pyglet
+pip install arcade pygame pillow opencv-python simplecv mahotas pytest selenium hypothesis tox
